5acd42767ae12fa33fbeb17d6c01bbd02600728b
Ronak Patel <rpatel4399@conestogac.on.ca>
2021-06-21 12:45:49 -0400

    game design

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import React,{ Component } from 'react';
+import './App.css';
+import Game from './tic-tac-toe';
+
+function App() {
+  return (
+    <Game></Game>
+  );
+}
+
+export default App;
